723caf4fd30775a51aacd08120f246b5f817d806
Ginny <virginiaegraybill@gmail.com>
2022-02-08 10:04:51 -0800

    minor changes
    FIX THE SLIDER it makes things awful and slow

--- Portfolio Website/js/js.js
+++ Portfolio Website/js/js.js
@@ -15,8 +15,6 @@ $('.nav').on('click', function(){
 });
 
 
-console.log($(window).width)
-
 function left(){
     $('.hero').transit({'background-position':'-50px'}, 7000);
 }
@@ -25,67 +23,69 @@ function right(){
 };
 
     
-    function sliderAuto() {
-        if($(window).width() < 900){
-            right();
+//     function sliderAuto() {
+//         if($(window).width() < 900){
+//             right();
+//             var my_image = new Image();
+//             my_image.src = 'img/hero/hero2.webp';
+//             $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero2.webp")', 'opacity':'100%'},2000);
+//             left();
 
-            $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero2.webp")', 'opacity':'100%'},2000);
-            left();
+//             my_image.src = 'img/hero/hero3.webp';
+//             $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero3.webp")', 'opacity':'100%'},2000);
+//             right();
 
-            $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero3.webp")', 'opacity':'100%'},2000);
-            right();
+//             $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero7.webp")', 'opacity':'100%'},2000);
+//             left();
 
-            $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero7.webp")', 'opacity':'100%'},2000);
-            left();
+//             $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero5.webp")', 'opacity':'100%'},2000);
+//             right();
 
-            $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero5.webp")', 'opacity':'100%'},2000);
-            right();
+//             $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero6.webp")', 'opacity':'100%'},2000);
+//             left();
 
-            $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero6.webp")', 'opacity':'100%'},2000);
-            left();
+//             $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero1.webp")', 'opacity':'100%'},2000);
+//         } else if ($(window).width() >= 900) {
+//             right();
 
-            $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero1.webp")', 'opacity':'100%'},2000);
-        } else if ($(window).width() >= 900) {
-            right();
+//             $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero2lg.webp")', 'opacity':'100%'},2000);
+//             left();
 
-            $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero2lg.webp")', 'opacity':'100%'},2000);
-            left();
+//             $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero3lg.webp")', 'opacity':'100%'},2000);
+//             right();
 
-            $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero3lg.webp")', 'opacity':'100%'},2000);
-            right();
+//             $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero7lg.webp")', 'opacity':'100%'},2000);
+//             left();
 
-            $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero7lg.webp")', 'opacity':'100%'},2000);
-            left();
+//             $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero5lg.webp")', 'opacity':'100%'},2000);
+//             right();
 
-            $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero5lg.webp")', 'opacity':'100%'},2000);
-            right();
+//             $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero6lg.webp")', 'opacity':'100%'},2000);
+//             left();
 
-            $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero6lg.webp")', 'opacity':'100%'},2000);
-            left();
+//             $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero1lg.webp")', 'opacity':'100%'},2000);
+//         }
 
-            $('.hero').transit({'opacity':'50%','background-image':'url("img/hero/hero1lg.webp")', 'opacity':'100%'},2000);
-        }
+//     }
 
-    }
-
-sliderAuto();
-setInterval(function(){
-    if (document.hasFocus()){
-    sliderAuto();
-    }
-},0)
+// sliderAuto();
+// setInterval(function(){
+//     if (document.hasFocus()){
+//     sliderAuto();
+//     }
+// },2000)
 
 $('article').on('mouseenter', function(){
     current=$(this);
     current.stop().transit({'background-size':'170%'}, 300);
     current.find('.overlay').stop().transit({'background':'rgba(42, 39, 64, 0.8)'}, 300);
-    current.find('h4').transit({'opacity':'100%'},300);
-    current.find('h4').parent().transit({'border':'2px solid aliceblue'},300);
+    current.find('h4').stop().transit({'opacity':'100%'},300);
+    current.find('h4').parent().stop().transit({'border':'2px solid aliceblue'},300);
 });
 $('article').on('mouseleave', function(){
     current=$(this);
     current.stop().transit({'background-size':'100%'}, 300);
     current.find('.overlay').stop().transit({'background':'transparent'}, 200)
-    current.find('h4').transit({'opacity':'0'},300);
-    current.find('h4').parent().transit({'border':'2px solid rgba(240, 248, 255, 0.445)'},300);
+    current.find('h4').stop().transit({'opacity':'0'},300);
+    current.find('h4').parent().stop().transit({'border':'2px solid rgba(240, 248, 255, 0.445)'},300);
 });
